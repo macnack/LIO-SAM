c11f3853144280ddd8e72f2ebe38e99659c1c37d
macnack <krupka.maciej@icloud.com>
2022-06-01 10:21:34 +0200

    Update featureExtraction.cpp

--- src/featureExtraction.cpp
+++ src/featureExtraction.cpp
@@ -1,26 +1,6 @@
 #include "utility.h"
 #include "lio_sam/cloud_info.h"
 
-<<<<<<< Updated upstream
-// initial by column, first: 
-Matching_Transform(0, 0) = -0.9931;
-Matching_Transform(0, 1) = 0.1168;
-Matching_Transform(0, 2) = -0.0133;
-// second column
-Matching_Transform(0, 0) = -0.1169;
-Matching_Transform(0, 1) = -0.9931;
-Matching_Transform(0, 2) = 0.0013;
-// 3th column
-Matching_Transform(0, 0) = -0.0131;
-Matching_Transform(0, 1) = 0.0028;
-Matching_Transform(0, 2) = 0.9999;
-// 4th column - translation 
-Matching_Transform(0, 0) = 0.0274;
-Matching_Transform(0, 1) = -0.1502;
-Matching_Transform(0, 2) = 0.0399;
-=======
->>>>>>> Stashed changes
-
 struct smoothness_t{ 
     float value;
     size_t ind;
@@ -288,13 +268,8 @@ public:
         // Transform extracted features by initial matching matrix
         pcl::PointCloud<PointType>::Ptr transformed_surfaceCloud(new pcl::PointCloud<PointType>());
         pcl::PointCloud<PointType>::Ptr transformed_cornerCloud(new pcl::PointCloud<PointType>());
-<<<<<<< Updated upstream
-        pcl::transformPointCloud(*cornerCloud, *transformed_cornerCloud, Matching_Transform);
-        pcl::transformPointCloud(*surfaceCloud, *transformed_surfaceCloud, Matching_Transform);
-=======
         pcl::tranformPointCloud(*cornerCloud, *transformed_cornerCloud, Matching_Transform)
         pcl::tranformPointCloud(*surfaceCloud, *transformed_surfaceCloud, Matching_Transform)
->>>>>>> Stashed changes
         // save newly extracted features
         cloudInfo.cloud_corner  = publishCloud(pubCornerPoints,  transformed_cornerCloud,  cloudHeader.stamp, lidarFrame);
         cloudInfo.cloud_surface = publishCloud(pubSurfacePoints, transformed_surfaceCloud, cloudHeader.stamp, lidarFrame);
